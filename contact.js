f7a50667085f0384321a4dcca5900f61f70ecd68
heco2900 <heriberto.jaimes10@gmail.com>
2022-05-22 23:22:53 -0500

    adding about page and contact page

---
+++ contact.js
@@ -0,0 +1,6 @@
+const placeForResults = document.getElementById("result")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
